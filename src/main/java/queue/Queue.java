854a42be9f24aab960caadf613bb436f840644ec
Marlon Calvo <marloncalv98@gmail.com>
2020-02-05 16:10:10 -0500

    remove tabs, cause github scusk

--- src/main/java/queue/Queue.java
+++ src/main/java/queue/Queue.java
@@ -1,7 +1,5 @@
 public interface Queue<T> {
-
-	public void add(T item);
-	public T poll();
-	public boolean isEmpty();
-	
+   public void add(T item);
+   public T poll();
+   public boolean isEmpty();
 }
