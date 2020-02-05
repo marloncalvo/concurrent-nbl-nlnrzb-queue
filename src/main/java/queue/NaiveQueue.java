10c02ec34c30c364bf11f37efe642b6337dde8ac
Marlon Calvo <marloncalv98@gmail.com>
2020-02-05 16:07:31 -0500

    add comments explaining what i did :)

--- src/main/java/queue/NaiveQueue.java
+++ src/main/java/queue/NaiveQueue.java
@@ -4,10 +4,13 @@ public class NaiveQueue<T> implements Queue<T> {
 
 	private final LinkedList<T> items = new LinkedList<>();
 
+	// For LL, this will always add to the end of the list
+	// as O(1) operation.
 	public void add(T item) {
 		items.add(item);
 	}
 
+	// For LL, removes from head of list, as O(1) operation.
 	public T poll() {
 		return items.remove();
 	}
